3f994672a4f21ce3c3ac47720a4580978743aeaa
Aldo Lizarde <alizarde@ids.com.mx>
2020-08-06 18:34:13 -0500

    se sube el build

---
+++ docs/precache-manifest.3e000bc26d8d679827b88b9cd38e5670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118fa32a9a29438286c9b809acc14d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48cc82fa56025fe5c58e",
+    "url": "/static/css/2.e0be0c20.chunk.css"
+  },
+  {
+    "revision": "48cc82fa56025fe5c58e",
+    "url": "/static/js/2.1b2a3874.chunk.js"
+  },
+  {
+    "revision": "92e24a7b7235b5a6ec9e013ea001216d",
+    "url": "/static/js/2.1b2a3874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9712738711aa14dfe18",
+    "url": "/static/js/main.e7ae4bf1.chunk.js"
+  },
+  {
+    "revision": "4275e298ca4247238cbd",
+    "url": "/static/js/runtime-main.a18a15fd.js"
+  }
+]);
